bd10f1c8f3cc5ca348ace43d267f81437480dc39
Orazalinov Askhat <58249226+askesaske@users.noreply.github.com>
2021-01-30 20:31:38 +0600

    adaptive finished and states added, need to complete functional part

--- src/js/app.js
+++ src/js/app.js
@@ -262,35 +262,22 @@ var swiper = new Swiper('.sales__swiper', {
 });
 
 function openCity(evt, cityName) {
-    // Declare all variables
     var i, tabcontent, tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("menu__tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
-    // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("menu__tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" menu__tablinks--active", "");
     }
 
-    // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " menu__tablinks--active";
 }
 
 new Vue({
-    el: '#menu',
-    data: {
-        modalToggle: false
-    },
 
-    methods: {
-        callModal() {
-            this.modalToggle = true;
-        }
-    },
 });
